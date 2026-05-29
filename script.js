c3944db5865c438586370a24344a2993307d252a
AkkunurNithin <akkunurnithin1234@gmail.com>
2026-05-29 14:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const container = document.querySelector(".container");
+
+        for(let i=0; i<800; i++){
+            const box = document.createElement("div")
+            box.className = "box";
+
+            box.addEventListener("mouseover", function(){
+                box.style.backgroundColor = getcolor()
+            });
+
+             box.addEventListener("mouseleave", function(){
+                setTimeout(()=> {
+                    box.style.backgroundColor = "gray"
+                }, 1000);
+            })
+            container.appendChild(box);
+
+            function getcolor () {
+                const color = "#" + Math.floor(Math.random() * 256 * 256 * 256).toString(16);
+                return color;
+            }
+        }
